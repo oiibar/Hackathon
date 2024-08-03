4a336f68406fd68195cff5e396148380e4e0a7ca
Aibar Tolegeno <aibartolegeno.2283@gmail.com>
2024-08-03 18:12:17 +0500

    deploy

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -37,9 +37,7 @@ const registerUser = (req, res) => {
 const loginUser = (req, res) => {
   try {
     res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
     res.setHeader(
       "Access-Control-Allow-Methods",
       "GET,OPTIONS,PATCH,DELETE,POST,PUT"
